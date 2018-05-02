ba5ccc4390956e8a6c5c6830224a9ecac4962300
Shawnkaku <shawnkuo67@gmail.com>
2018-05-02 16:44:34 +0800

    Complete post actions and the form partial.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,55 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy, :edit]
+
   def index
     @posts = Post.all
   end
+
   def show
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = User.first
+
+    if @post.save
+      flash[:notice] = "Your post was created!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+
+  end
+  def set_post
     @post = Post.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
-  resources :posts, :except => [:destroy]
+  resources :posts, except: [:destroy]
 end
