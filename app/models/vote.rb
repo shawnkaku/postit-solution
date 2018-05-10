be4814c62a429d7635e6c90150adc0214a10ab97
Shawnkaku <shawnkuo67@gmail.com>
2018-05-10 10:03:03 +0800

    Comment Votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
 
   validates_uniqueness_of :creator, scope: :voteable
+  # validates_uniqueness_of :creator, scope: [:voteable_id, :voteable_type]
 end
